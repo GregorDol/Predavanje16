9f9e32e478e9e8f336e601098c55f01a9d528368
Gregor789 Dolinar <gregordolinar97@gmail.com>
2019-11-07 17:53:38 +0100

    Dodali smo kontaktno formo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,6 +12,17 @@ def prva_stran():
 def kontakt():
     emaili = ["ime@example.com", "ime@gmail.com", "ime@hotmail.com"]
     return render_template("kontakt.html", emaili=emaili)
+
+@app.route("/poslji-sporocilo", methods=["POST"])
+def poslji_sporocilo():
+    zadeva = request.form.get("zadeva")
+    sporocilo = request.form.get("sporocilo")
+
+    #Tukaj bi shranili te spremenlkjivki v bazo.
+
+    print("zadeva je:"  + zadeva)
+    return "Hvala za poslano zadevo: " + zadeva
+
 
 @app.route("/o_meni")
 def o_meni():
